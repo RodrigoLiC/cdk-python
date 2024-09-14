9667603e7fbe652d9e40e31f28994607ff8a743e
RodrigoLiC <165910033+RodrigoLiC@users.noreply.github.com>
2024-09-14 16:00:11 -0500

    Update cdk_python_stack.py

--- cdk_python/cdk_python_stack.py
+++ cdk_python/cdk_python_stack.py
@@ -12,6 +12,10 @@ class CdkPythonStack(Stack):
         synthesizer = DefaultStackSynthesizer(
             file_assets_bucket_name="cf-templates-1k0oguavt109r-us-east-1",  # Nombre del bucket S3 para los archivos de activos
             deploy_role_arn="arn:aws:iam::239245338181:role/LabRole"  # Rol de IAM que se usará para desplegar
+            cloud_formation_execution_role= "arn:aws:iam::239245338181:role/LabRole",
+            file_asset_publishing_role_arn="arn:aws:iam::239245338181:role/LabRole",
+            deploy_role_external_id="arn:aws:iam::239245338181:role/LabRole",
+            image_asset_publishing_role_arn="arn:aws:iam::239245338181:role/LabRole",
         )
         
         # Llamar al constructor de Stack y pasar el sintetizador
@@ -46,3 +50,56 @@ class CdkPythonStack(Stack):
     
         CfnOutput(self, "InstanceId", value=instance.instance_id, description="ID de la instancia EC2")
         CfnOutput(self, "InstancePublicIP", value=instance.instance_public_ip, description="IP pública de la instancia")
+        
+
+        
+""" 
+# Parámetros (nombre de instancia y AMI ID)
+        instance_name = "MV Reemplazar"  # Nombre por defecto de la instancia
+        ami_id = "ami-0aa28dab1f2852040"  # AMI ID especificada
+        
+        # Crear un Security Group
+        security_group = ec2.SecurityGroup(self, "InstanceSecurityGroup",
+            vpc=ec2.Vpc.from_lookup(self, "DefaultVPC", is_default=True),  # Usar la VPC por defecto
+            description="Permitir tráfico SSH, HTTP y en el puerto 8000 desde cualquier lugar",
+            allow_all_outbound=True
+        )
+
+# Agregar reglas de ingreso al Security Group
+        security_group.add_ingress_rule(
+            peer=ec2.Peer.any_ipv4(),
+            connection=ec2.Port.tcp(22),
+            description="Permitir SSH desde cualquier lugar"
+        )
+        
+        security_group.add_ingress_rule(
+            peer=ec2.Peer.any_ipv4(),
+            connection=ec2.Port.tcp(80),
+            description="Permitir HTTP desde cualquier lugar"
+        )
+
+        security_group.add_ingress_rule(
+            peer=ec2.Peer.any_ipv4(),
+            connection=ec2.Port.tcp(8000),
+            description="Permitir tráfico en el puerto 8000 desde cualquier lugar"
+        )
+
+        # Crear una instancia EC2
+        ec2_instance = ec2.Instance(self, "EC2Instance",
+            instance_type=ec2.InstanceType("t2.micro"),
+            machine_image=ec2.MachineImage.generic_linux({
+                "us-east-1": ami_id  # Especificar la AMI en la región
+            }),
+            vpc=ec2.Vpc.from_lookup(self, "DefaultVPC", is_default=True),
+            key_name="vockey",  # El nombre del par de claves
+            security_group=security_group,
+            block_devices=[ec2.BlockDevice(
+                device_name="/dev/sda1",
+                volume=ec2.BlockDeviceVolume.ebs(20)  # Tamaño del volumen en GB
+            )],
+        )
+         # Salidas
+        CfnOutput(self, "InstanceId", value=ec2_instance.instance_id, description="ID de la instancia EC2")
+        CfnOutput(self, "InstancePublicIP", value=ec2_instance.instance_public_ip, description="IP pública de la instancia")
+
+           """
