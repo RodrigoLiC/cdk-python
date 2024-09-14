634e9054ae30284cd8db09985cc52e091dbd965c
RodrigoLiC <165910033+RodrigoLiC@users.noreply.github.com>
2024-09-14 14:26:40 -0500

    Update cdk_python_stack.py

--- cdk_python/cdk_python_stack.py
+++ cdk_python/cdk_python_stack.py
@@ -6,9 +6,40 @@ from aws_cdk import (
 from constructs import Construct
 
 class CdkPythonStack(Stack):
-
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
+
+        # VPC
+        existing_vpc = ec2.Vpc.from_vpc_attributes(
+            self, 'ExistingVPC',
+            vpc_id='vpc-0d6ed0ab35089f55a',
+            availability_zones=['us-east-1a', 'us-east-1c'],
+            public_subnet_ids=['subnet-068e54de0032f5a1f', 'subnet-0056d79a26a2b99cc']
+        )
+
+        # Security Group
+        security_group1 = ec2.SecurityGroup.from_security_group_id(
+            self, 'SG1', 'sg-0f6b89062a9d16e2c'
+        )
+
+        # instancia EC2
+        instance = ec2.Instance(
+            self,
+            "cdkPython", # Nombre de la instancia
+            instance_type=ec2.InstanceType("t2.micro"),
+            machine_image=ec2.MachineImage.generic_linux({
+                "us-east-1": "ami-0aa28dab1f2852040"
+            }),
+            vpc=existing_vpc,
+            security_group=security_group1,
+            associate_public_ip_address=True,
+            key_name="ejemplo"
+        )
+
+
+
+
+        
 
         # Parámetros (nombre de instancia y AMI ID)
         instance_name = "MV Reemplazar"  # Nombre por defecto de la instancia
@@ -21,7 +52,12 @@ class CdkPythonStack(Stack):
             allow_all_outbound=True
         )
         
-        # Agregar reglas de ingreso al Security Group
+
+        # Salidas
+        CfnOutput(self, "InstanceId", value=ec2_instance.instance_id, description="ID de la instancia EC2")
+        CfnOutput(self, "InstancePublicIP", value=ec2_instance.instance_public_ip, description="IP pública de la instancia")
+
+""" # Agregar reglas de ingreso al Security Group
         security_group.add_ingress_rule(
             peer=ec2.Peer.any_ipv4(),
             connection=ec2.Port.tcp(22),
@@ -53,8 +89,4 @@ class CdkPythonStack(Stack):
                 device_name="/dev/sda1",
                 volume=ec2.BlockDeviceVolume.ebs(20)  # Tamaño del volumen en GB
             )],
-        )
-
-        # Salidas
-        CfnOutput(self, "InstanceId", value=ec2_instance.instance_id, description="ID de la instancia EC2")
-        CfnOutput(self, "InstancePublicIP", value=ec2_instance.instance_public_ip, description="IP pública de la instancia")
+        ) """
